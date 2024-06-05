cc67ee078cba8c758a7fb1cf6b5e21883848f1ae
Rush2128 <russellung@gmail.com>
2024-06-06 01:11:21 +1000

    sample

---
+++ frontend/src/pages/HomePage.js
@@ -0,0 +1,13 @@
+// src/pages/HomePage.js
+import React from 'react';
+import { Box, Heading } from '@chakra-ui/react';
+
+const HomePage = () => {
+  return (
+    <Box p={4}>
+      <Heading>Welcome to the Capstone Projects Management Platform</Heading>
+    </Box>
+  );
+};
+
+export default HomePage;
